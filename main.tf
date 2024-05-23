cb5908333e50770da2a78758ba95ef98957542d1
James Bayer <1139532+jbayer@users.noreply.github.com>
2024-05-23 12:25:20 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    vault = {
+      source = "hashicorp/vault"
+      version = "4.2.0"
+    }
+    hcp = {
+      source  = "hashicorp/hcp"
+      version = "0.89"
+    }
+  }
+}
+
+provider "vault" {
+  address = var.vault_addr
+  token   = var.VAULT_TOKEN
+  namespace = var.namespace
+}
+
+provider "hcp" {
+  project_id = var.project_id
+}
+
+data "hcp_vault_secrets_app" "vault-ent" {
+  app_name = var.app_name
+}
+
+resource "vault_kv_secret_v2" "from_hvs" {
+  mount                      = "sync"
+  name                       = "from-hvs"
+  data_json                  = jsonencode(data.hcp_vault_secrets_app.vault-ent.secrets)
+  custom_metadata {
+    max_versions = 2
+    data = {
+      from_hvs = true
+      project_id = var.project_id
+      app = var.app_name
+    }
+  }
+}
